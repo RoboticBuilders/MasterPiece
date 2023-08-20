7e19f40317fffed98e3725b80902300c3a045cec
Anya <anya.rajyaguru@gmail.com>
2023-08-20 11:42:14 -0700

    async support and fixing ports

--- Utilities.py
+++ Utilities.py
@@ -1,5 +1,5 @@
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor
+from pybricks.pupdevices import Motor, ColorSensor
 from pybricks.parameters import Direction, Port
 from pybricks.robotics import GyroDriveBase, DriveBase
 
@@ -8,16 +8,20 @@ right_motor = Motor(Port.E)
 left_med_motor = Motor(Port.B)
 right_med_motor = Motor(Port.D)
 
+# Need to confirm
+left_color = ColorSensor(Port.A)
+right_color = ColorSensor(Port.F)
+
 hub = PrimeHub()
 
-robot = DriveBase(
+robot = GyroDriveBase(
     left_motor,
     right_motor,
     wheel_diameter=88,
     axle_track= 122)
 
 DEFAULT_SPEED = 300
-DEFAULT_ACCELERATION = 100
+DEFAULT_ACCELERATION = 500
 DEFAULT_TURN_RATE = 300
 DEFAULT_TURN_ACCEL = 100
 
@@ -28,32 +32,41 @@ def resetRobot():
 def resetGyro(angle:int = 0):
     hub.imu.reset_heading(angle)
 
-async def goStraight(distance, backward = False, straightSpeed=DEFAULT_SPEED, straightAcceleration=DEFAULT_ACCELERATION, turnRate=DEFAULT_TURN_RATE, turnAcceleration=DEFAULT_TURN_ACCEL, wait=True):
+def goStraight(distance, wait=True, backward = False, straightSpeed=DEFAULT_SPEED, straightAcceleration=DEFAULT_ACCELERATION, turnRate=DEFAULT_TURN_RATE, turnAcceleration=DEFAULT_TURN_ACCEL):
     """
     Drives the robot straight for a specified distance.
 
     distance -- Distance to travel in mm
-    backward -- [Optional, default=false] set to True to move backwards, else robot will move forward
+
+    wait -- [Optional, default=True] wait for action to finish before returning from function
+
+    backward -- [Optional, default=False] set to True to move backwards, else robot will move forward
+
     straightSpeed -- [Optional, default= (Number, mm/s) Straight-line speed of the robot.
+
     straightAcceleration -- (Number, mm/s²) Straight-line acceleration and deceleration of the robot. Provide a tuple with two values to set acceleration and deceleration separately.
+
     turnRate -- (Number, deg/s) Turn rate of the robot.
+
     turnAcceleration (Number, deg/s²) Angular acceleration and deceleration of the robot. Provide a tuple with two values to set acceleration and deceleration separately.
-    wait -- 
     """
     (origSpeed, origAccel, origTurnSpeed, origTurnAccel) = robot.settings()
     robot.settings(straightSpeed, straightAcceleration, turnRate, turnAcceleration)
 
     if(backward):
-        robot.straight(-1*distance, wait)
+        robot.straight(-1*distance, wait=wait)
     else: 
-        robot.straight(distance, wait)
+        robot.straight(distance, wait=wait)
     
     robot.settings(origSpeed, origAccel, origTurnSpeed, origTurnAccel)
 
-async def turnToAngle(absoluteAngle:int, wait=True):
+def turnToAngle(absoluteAngle:int, wait=True):
     """
     Turns the robot to the specific Gyro angle
+
     absoluteAngle -- Target angle the robot should get to (range -179 to 180)
+
+    wait -- [Optional, default=True] wait for action to finish before returning from function
     """
-    angleToTurn = absoluteAngle - robot.imu.heading()
+    angleToTurn = absoluteAngle - robot.angle()
     robot.turn(angleToTurn)
