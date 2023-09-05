882b736a20fd4160f91af24c9002e309f8bb99df
Anya <anya.rajyaguru@gmail.com>
2023-09-05 11:07:11 -0700

    Runs and Utilities

--- ArishaRun.py
+++ ArishaRun.py
@@ -5,58 +5,29 @@ from pybricks.robotics import GyroDriveBase, DriveBase
 from pybricks.tools import wait, StopWatch
 from Utilities import *
 
-MM_PER_INCH = 25.4
-
-
-
-# def AnyaRun():
-#     resetRobot()
-#     goStraight(450)
-#     # robot.straight(450)
-#     turnToAngle(-50)
-#     # robot.turn(-50)
-#     goStraight(280)
-#     # robot.straight(290)
-#     wait(10000)
-#     robot.turn(87)
-#     robot.settings(straight_speed=180, turn_rate=180)
-#     robot.straight(200)
-#     robot.settings(straight_speed=300, turn_rate=300)
-#     left_med_motor.run_angle(-1000, 2600)
-#     robot.straight(-240)
-#     robot.turn(100)
-#     robot.straight(850)
-
-def AnyaRun():
-    goStraight(MM_PER_INCH*20)
-    turnToAngle(-45)
-    driveTillLine(speed=300, distanceInCM=200, target_angle=-45, doCorrection=False)
-    # goStraight(MM_PER_INCH*2)
-    turnToAngle(45)
-    goStraight(MM_PER_INCH*7.5)
-    left_med_motor.run_angle(-1000, 2500)
-    goStraight(MM_PER_INCH*-5)
-    turnToAngle(-20)
-    goStraight(MM_PER_INCH*-30)
+_MM_PER_INCH = 25.4
 
 def ArishaRun_M2():
-    goStraight(600) 
+    goStraight(600, straightSpeed=500) 
     turnToAngle(30) 
     goStraight(100) 
     turnToAngle(-45) 
-    #drive_base.settings(straight_speed=100, turn_rate=180)
 
     # turn back and forward
     goStraight(150)
     goStraight(-50)
-    right_med_motor.run_angle(500, -2350) 
+    right_med_motor.run_angle(1000, -1500) 
     goStraight(50)
-    # right_med_motor.run_angle(500, -2300) 
-    goStraight(-100)
+
     #go back
-    turnToAngle(absoluteAngle=45, turnRate=100, turnAcceleration=100) 
     goStraight(-100)
-    right_med_motor.run_angle(500, 2350) 
+    turnToAngle(absoluteAngle=30, turnRate=100, turnAcceleration=100) 
+    goStraight(-600, straightSpeed=500)
+    # turnToAngle(absoluteAngle=0, turnRate=100, turnAcceleration=100) 
+    # goStraight(-300) 
+    # turnToAngle(absoluteAngle=30, turnRate=100, turnAcceleration=100) 
+    # goStraight(-300) 
+    right_med_motor.run_angle(1000, 1500) 
 
 def ArishaRun_M1():
     left_med_motor.run_angle(800, 100)
@@ -69,8 +40,78 @@ def ArishaRun_M1():
 
 
 
+def ArishaRun_M1andM2():
+    left_med_motor.run_angle(200, 300)
+    goStraight(300)
+    #right_med_motor.run_angle(800,200)
+    left_med_motor.run_angle(200,-300)
+    left_med_motor.run_angle(200,300)
+
+    goStraight(-300)
+    #right_med_motor.run_angle(800,-200)
+    #go to M2
+    turnToAngle(30) 
+    goStraight(415, straightSpeed=500) 
+    turnToAngle(0) 
+    goStraight(200) 
+    left_med_motor.run_angle(800,300)
+    turnToAngle(-45) 
+
+    # turn back and forward
+    goStraight(150)
+    goStraight(-50)
+    right_med_motor.run_angle(1000, -1300) 
+    goStraight(50)
+
+    #go back
+    goStraight(-75)
+    turnToAngle(absoluteAngle=0, turnRate=100, turnAcceleration=100) 
+    left_med_motor.run_angle(800,-300)
+    goStraight(-600, straightSpeed=500)
+    right_med_motor.run_angle(1000, 1300) 
+
+def staticAttachmentTest():
+    # # Everything with one comment is in the orignal code!!!
+    goStraight(15*_MM_PER_INCH)
+    turnToAngle(45)
+    turnToAngle(0)
+    # goStraight(-5*MM_PER_INCH)
+    goStraight(-300)
+    #right_med_motor.run_angle(800,-200)
+    #go to M2 
+    turnToAngle(45) 
+    goStraight(415, straightSpeed=500) 
+    turnToAngle(0) 
+    goStraight(200) 
+    left_med_motor.run_angle(800,300)
+    turnToAngle(-45) 
+
+    # turn back and forward
+    goStraight(150)
+    goStraight(-50)
+    right_med_motor.run_angle(1000, -1300) 
+    goStraight(50)
+
+    #go back
+    goStraight(-75)
+    turnToAngle(absoluteAngle=0, turnRate=100, turnAcceleration=100) 
+    left_med_motor.run_angle(800,-300)
+    goStraight(-600, straightSpeed=500)
+    right_med_motor.run_angle(1000, 1300) 
+
+
+
 print("Calling func now")
-ArishaRun_M1()
+
+
+stopwatch = StopWatch()
+start_time = stopwatch.time()#time.ticks_ms()  
+#ArishaRun_M2()
+ArishaRun_M1andM2()
+#staticAttachmentTest()
+end_time = stopwatch.time()
+#logMessage("Time for run {} time(ms): {}".format(str(run), str(time.ticks_diff(end_time, start_time))), level=0)
+print(str(start_time), str(end_time))
 # AnyaRun()
 # robot.straight(distance=500, then=Stop.Hold, wait=True)
 # left_med_motor.run_angle(-1000, 2600)
