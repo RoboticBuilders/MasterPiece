bb4bf38ec31e12c52b57a7905e8c045c903f3d03
Rishabh <johndoe@example.com>
2023-09-24 16:58:12 -0700

    Changes to run1

--- VirtualReality.py
+++ VirtualReality.py
@@ -1,1 +1,9 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import GyroDriveBase, DriveBase
+from pybricks.tools import wait, StopWatch
 from Utilities import *
+def virtualRealityArtist():
+    left_med_motor.run_angle(700,-3300)
+virtualRealityArtist()
---
+++ run1.py
@@ -0,0 +1,5 @@
+from Utilities import *
+
+def test3DCinema():
+    gyroStraightWithDrive(distance = 32, speed = 35)
+    turnToAngle(targetAngle = 20, speed = 200)
