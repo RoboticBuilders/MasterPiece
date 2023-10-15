423b40a4ef32185ed3cf2b5100ebc9bfa3b11fae
Rishabh <johndoe@example.com>
2023-10-15 16:55:43 -0700

    Deleting unused files

--- run1.py
+++
@@ -1,7 +0,0 @@
-from Utilities import *
-
-def test3DCinema():
-    gyroStraightWithDrive(distance = 32, speed = 35)
-    turnToAngle(targetAngle = 20, speed = 200)
-def threeDCinemaTest():
-    gyroStraightWithDrive(distanceInCm = 32, speed = 35)
