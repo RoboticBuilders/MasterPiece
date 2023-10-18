7ee0783b6ce6b4bb50d24bb25120c2a506d79dd8
nhthaker <91383651+nhthaker@users.noreply.github.com>
2023-10-17 18:34:46 -0700

    Helps explain the run.

--- AARun3.py
+++ AARun3.py
@@ -7,6 +7,7 @@ from Utilities import *
 
 def run3():
 
+    # This function drops of the innovation project, expert, and orange person to the museum.
     def _positionInnovationProjectAndExpertsToMuseum():
 
         turnToAngle(330, speed=200, oneWheelTurn=True)
@@ -20,7 +21,7 @@ def run3():
 
         turnToAngle(270, speed=200, oneWheelTurn=True)
         wait(500)
-
+    # This function does the lightshow mission by lifting up the arms.
     def _executeLightShow():
         gyroStraightWithDrive(distanceInCm=21, speed=200, targetAngle=270, backward=True)
 
@@ -30,7 +31,7 @@ def run3():
         left_med_motor.run_angle(600, 360 * 4)
 
         left_med_motor.run_angle(300, -360 * 4)
-
+    # This function does the immersive experience mission.
     def _executeImmersiveExperience():
         gyroStraightWithDrive(distanceInCm=7, speed=300, targetAngle=270)
 
@@ -42,14 +43,14 @@ def run3():
         turnToAngle(270, speed=200)
 
         gyroStraightWithDrive(distanceInCm=23, speed=300, targetAngle=270)
-
+    # This function is when we go to homw 2 and we pick up Emily on the way back.
     def _goHomeWithEmily():
         gyroStraightWithDrive(distanceInCm=50, speed=300, targetAngle=270, backward=True)
 
         turnToAngle(15, speed=200)
 
         gyroStraightWithDrive(distanceInCm=110, speed=341, targetAngle=15)
-
+    # This is function runs the entire run.
     def _codeForRun3():
         resetGyro(0)
         _positionInnovationProjectAndExpertsToMuseum()
