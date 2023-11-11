a968714ae4708daa46835679626cfa09792ffc9c
Ashish Kalra <ashish.akalra@hotmail.com>
2023-11-11 13:26:56 -0800

    final attachment for the run2

--- run2.py
+++ run2.py
@@ -6,98 +6,34 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
-def soundmixer_sidebucketarm():
-    gyroStraightWithDrive(distanceInCm = 30, speed = 500, targetAngle = 0)
-    gyroStraightWithDrive(distanceInCm = 12, speed = 200, targetAngle = 0)
+def run2_testSide():
+    short_bucket_rotation_angle = 300
+    short_bucket_rotation_angle = 600
 
-    #gyroStraightWithDrive(distanceInCm = 18, speed = 200, targetAngle = 0)
-    #wait(1000)
-    turnToAngle(targetAngle=100, speed=50)
-    #wait(1000)
+    #go to the movie set
+    gyroStraightWithDrive(distanceInCm=51, speed=350)
+    right_med_motor.run_angle(600, rotation_angle=-200)
+    
+    #get camera in right position
+    gyroStraightWithDrive(distanceInCm=18, speed=200, backward=True)
+    turnToAngle(targetAngle=315, speed=40)
+    right_med_motor.run_angle(600, rotation_angle=100)
+    right_med_motor.run_angle(600, rotation_angle=250, wait=False)
 
-def goHome():
-    gyroStraightWithDrive(distanceInCm = 5, speed = 200, targetAngle = 0, backward=True)
-    turnToAngle(targetAngle=0, speed=50)
-    gyroStraightWithDrive(distanceInCm = 40, speed = 500, targetAngle = 0, backward=True)
+    #go back to align for sound mixer
+    gyroStraightWithDrive(distanceInCm=26, speed=200, backward=True, targetAngle=0)
 
-def moviesetandlever_sidebucketarm():
-    gyroStraightWithDrive(distanceInCm=24, speed=100, slowDown=True)
-    turnToAngle(targetAngle=45, speed=250)
-    gyroStraightWithDrive(distanceInCm=9, speed=100)
-    right_med_motor.run_angle(speed=400, rotation_angle=-250)
-    turnToAngle(targetAngle=25, speed=400)
-    gyroStraightWithDrive(distanceInCm=-3, speed=250)
-    gyroStraightWithDrive(distanceInCm=10, speed=200, backward=True)
-    turnToAngle(targetAngle=330, speed=250)
-    right_med_motor.run_angle(speed=2, rotation_angle=250)
-    gyroStraightWithDrive(distanceInCm=5, speed=200, backward=True)
-    turnToAngle(targetAngle=330, speed=250)
-    gyroStraightWithDrive(distanceInCm=30, speed=500)
- 
-def goto_soundmixer_bigbucket():
-    gyroStraightWithDrive(distanceInCm = 41, speed = 200, backward = False, targetAngle = 0, slowDown = True)
+    #set angle to sound mixer
+    turnToAngle(targetAngle=300, speed=40)
 
-def solve_soundmixer_bigbucket():
-    right_med_motor.run_angle(speed=500, rotation_angle=-1500)
-    gyroStraightWithDrive(distanceInCm = 3, speed = 200, backward = False, targetAngle = 0, slowDown = True)
-    #right_med_motor.run_angle(speed=500, rotation_angle=-200)
-    turnToAngle(targetAngle=20, speed=30)
-    turnToAngle(targetAngle=120, speed=50)
+    #go to sound mixer
+    gyroStraightWithDrive(distanceInCm=45, speed=250)
+    right_med_motor.run_angle(600, rotation_angle=-600)
 
-def goto_movieset_bigbucket():
-    gyroStraightWithDrive(distanceInCm = 32, speed = 200, slowDown = True)
-    turnToAngle(targetAngle=45, speed=50)
-    gyroStraightWithDrive(distanceInCm = 8, speed = 100, slowDown = True)
-
-def solve_movieset_bigbucket():
-    right_med_motor.run_angle(speed=600, rotation_angle=1500)
-    gyroStraightWithDrive(distanceInCm = 12, speed = 200, backward = True, targetAngle = 45, slowDown = True)
-    turnToAngle(targetAngle=0, speed=50)
-    right_med_motor.run_angle(speed=600, rotation_angle=-500)
-
-def comehome_bigbucket():
-    turnToAngle(targetAngle=45, speed=50)
-    gyroStraightWithDrive(distanceInCm = 45, speed = 200, backward = True, targetAngle = 45, slowDown = False)
-
-def run2_sidebucketarm():
-    #initializeAndWaitForRobotReady()
-    soundmixer_sidebucketarm()
-    goHome()
-    #moviesetandlever_sidebucketarm()
-
-def run2_bigbucket():
-    #initializeAndWaitForRobotReady()
-    goto_soundmixer_bigbucket()
-    solve_soundmixer_bigbucket()
-    goto_movieset_bigbucket()
-    solve_movieset_bigbucket()
-    comehome_bigbucket()
-
-def Run2New():
-    initializeAndWaitForRobotReady()
-    #soundmixer
-    gyroStraightWithDrive(distanceInCm=51, speed=200)
-    turnToAngle(targetAngle=20, speed=40)
-    turnToAngle(targetAngle=120, speed=300)
-
-    #going to movie set
-    gyroStraightWithDrive(distanceInCm=20, speed=300)
-    turnToAngle(targetAngle=48, speed=300)
-    gyroStraightWithDrive(distanceInCm=3, speed=300)
-
-
-    #bringing camera to target
-    right_med_motor.run_angle(speed=400, rotation_angle=-350)
-    gyroStraightWithDrive(distanceInCm=15, speed=200, backward=True)
-    turnToAngle(targetAngle=20)
-    #gyroStraightWithDrive(distanceInCm=18, speed=300, backward=True) # this flicks the lever too
-    #turnToAngle(targetAngle=5, speed=300)
-
-    #go to home
-    right_med_motor.run_angle(300, rotation_angle=350.)
-    gyroStraightWithDrive(distanceInCm=40, speed=500, backward=True)
+    #come back home
+    gyroStraightWithDrive(distanceInCm=45, speed=500, backward=True, targetAngle=0)
+    #right_med_motor.run_angle(speed=600, rotation_angle=600)
 
 def run2():
     resetGyro(0)
-    Run2New()
-    #run2_sidebucketarm()
+    run2_testSide()
