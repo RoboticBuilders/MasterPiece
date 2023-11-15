1ffb1231fafd223b8c3d18e66a5d737f5d2aee8b
Anya <anya.rajyaguru@gmail.com>
2023-11-14 19:58:00 -0800

    commenting run2_Arisha

---
+++ run2_Arisha.py
@@ -0,0 +1,29 @@
+# from Utilities import *
+# from pybricks.parameters import Stop
+# from pybricks.hubs import PrimeHub
+# from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+# from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+# from pybricks.robotics import DriveBase
+# from pybricks.tools import wait, StopWatch
+
+# def run2_testSide():
+#     gyroStraightWithDrive(20.5*CM_PER_INCH, speed=350,targetAngle=0)
+#     right_med_motor.run_angle(speed=800, rotation_angle = -1300)
+#     gyroStraightWithDrive(distanceInCm=-18, speed=200)
+#     turnToAngle(targetAngle=315, speed=40)
+#     right_med_motor.run_angle(speed=800, rotation_angle = 1300)
+#     gyroStraightWithDrive(-8*CM_PER_INCH, speed=350,targetAngle=0)
+#     # turnToAngle()
+
+
+
+
+# def run2():
+#     resetGyro(0)
+#     run2_testSide()
+
+
+
+
+
+# # run2()
