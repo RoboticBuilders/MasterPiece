631b619d4d907280cb9f5ee82e75462d8f403c86
Anya <anya.rajyaguru@gmail.com>
2023-11-21 17:32:22 -0800

    removed parallel from sound mixer

--- run2.py
+++ run2.py
@@ -9,11 +9,11 @@ from pybricks.tools import wait, StopWatch
 def run2():
     resetGyro(0)
     gyroStraightWithDrive(distanceInCm=38, speed=350)
-    right_med_motor.run_angle(speed=2000, rotation_angle = -1300, wait=False) # was 1000
-    gyroStraightWithDrive(distanceInCm=20, speed=250)
-
+    #right_med_motor.run_angle(speed=1000, rotation_angle = -1300, wait=False) # was 1000
+    gyroStraightWithDrive(distanceInCm=20, speed=250) 
+    right_med_motor.run_angle(speed=2000, rotation_angle = -1300)
     #come back home
     gyroStraightWithDrive(distanceInCm=48, speed=500, backward=True, targetAngle=0)
     right_med_motor.run_angle(speed=2000, rotation_angle = 1300)
 
-#run2()
+run2()
