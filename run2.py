e7cf4c191ed744cbe2023c28dac34323e76ccbdf
Anya <anya.rajyaguru@gmail.com>
2023-11-22 13:23:20 -0800

    run2 improve accuracy

--- run2.py
+++ run2.py
@@ -10,10 +10,30 @@ def run2():
     resetGyro(0)
     gyroStraightWithDrive(distanceInCm=38, speed=350)
     #right_med_motor.run_angle(speed=1000, rotation_angle = -1300, wait=False) # was 1000
-    gyroStraightWithDrive(distanceInCm=20, speed=250) 
-    right_med_motor.run_angle(speed=2000, rotation_angle = -1300)
+    gyroStraightWithDrive(distanceInCm=10, speed=250) 
+    right_med_motor.run_angle(speed=2000, rotation_angle = -1300, wait= False)
+    gyroStraightWithDrive(distanceInCm=7, speed=250) 
     #come back home
+    # total was 48
+    # gyroStraightWithDrive(distanceInCm=27, speed=500, backward=True, targetAngle=0)
+    # robot.turn(45)
+    # robot.straight(-100)
     gyroStraightWithDrive(distanceInCm=48, speed=500, backward=True, targetAngle=0)
     right_med_motor.run_angle(speed=2000, rotation_angle = 1300)
 
-#run2()
+# This is code that was outside.
+def initializeRun2():
+    print("Calling func now")
+
+    resetRobot()
+    stopwatch = StopWatch()
+    start_time = stopwatch.time()
+
+    #oldrun1()
+    run2()
+    end_time = stopwatch.time()
+    print("Time is " + str((end_time-start_time)/1000) + " seconds")
+
+    print("DONE")
+
+initializeRun2()
