0e99420ec525eb8fb761d8e76ca1f2acd179ca87
Ashish Kalra <ashish.akalra@hotmail.com>
2023-11-21 23:21:27 -0800

    Changes to do one image at a time based on bluetooth input...

--- innovation_project/4x4 Tactile Display/src/Master/Master.ino
+++ innovation_project/4x4 Tactile Display/src/Master/Master.ino
@@ -2,8 +2,26 @@
 #include <LiquidCrystal_I2C.h>
 #include <stdlib.h>
 
-int images[5][4][4] = 
-                  {                    
+int images[6][4][4] = 
+                  { 
+                    {
+                      { 0, 0, 0, 0},
+                      { 0, 0, 0, 0},
+                      { 0, 0, 0, 0},
+                      { 0, 0, 0, 0}
+                    },
+                    {
+                      { 0, 0, 0, 0},
+                      { 3, 3, 3, 3},
+                      { 3, 3, 3, 3},
+                      { 0, 0, 0, 0}
+                    },                   
+                    {
+                      { 0, 3, 3, 0},
+                      { 3, 0, 0, 3},
+                      { 3, 0, 0, 3},
+                      { 0, 3, 3, 0}
+                    },                   
                     {
                       { 3, 0, 0, 0},
                       { 3, 0, 3, 0},
@@ -24,10 +42,12 @@ int images[5][4][4] =
                     }
                   };
 
-int imageCount = 3;
+int imageIndex = 0;
 
 // Index of the image to display
-int imageIndex = 0;
+int lastInput = -1;
+
+bool imageRendered = false;
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -35,10 +55,12 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup()
 {
 	// initialize the LCD and turn on backlight
-	//lcd.begin();
-	//lcd.backlight();
+	lcd.begin();
+	lcd.backlight();
   
   WriteLcdAndWait("Reset pins", 12);
+
+  Serial.begin(9600);
 }
 
 void WriteLcdAndWait(String text, int waitTimeSeconds) {
@@ -53,20 +75,46 @@ void WriteLcdAndWait(String text, int waitTimeSeconds) {
 }
 
 void loop() {  
+ 
+  // code for bluetooth
+  int data = Serial.read();
+  Serial.println(String(data));
+
+  if (lastInput == data)
+  {
+    // entry for rendering the same image as before
+    return;
+  }
+
+  lastInput = data;
+
+  if (data == 0)
+  {
+    imageIndex = 1;
+    Serial.println("rectangle");
+  }
+  else if (data == 1)
+  {
+    imageIndex = 2;
+    Serial.println("circle");
+  }
+  else
+  {
+    // Image data to reset pins
+    imageIndex = 0;
+    Serial.println("Reset pins - clear");
+  }
+
   // Send height instructions to each sub unit
-  WriteToWire(0, 8);
-  WriteToWire(1, 9);
-  WriteToWire(2, 10);
-  WriteToWire(3, 11);
+  WriteToWire(0, 20);
+  WriteToWire(1, 21);
+  WriteToWire(2, 22);
+  WriteToWire(3, 23);
 
   // Wait for the next cycle
   WriteLcdAndWait("Sent image: " + String(imageIndex), 20);
 
-  // Increment image index, and reset to zero if needed.
-  imageIndex++;
-  if (imageIndex == imageCount) 
-    imageIndex = 0; 
-
+  delay(1000);
 }
 
 void WriteToWire(int row, int wireAddress) {
