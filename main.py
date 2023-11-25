62f38f5a786e71a9699b4702b970ab6aac23d845
Anya <anya.rajyaguru@gmail.com>
2023-11-25 15:22:30 -0800

    Changes to accurately measure total run time in main.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def _maindriver():
     arm_change_start_time = 0
     arm_change_end_time = 0
     stopwatch = StopWatch()
+    # This start time is going to get overridden in the main loop.
+    start_time = stopwatch.time()
 
     while True:
         hub.display.number(counter)
@@ -27,25 +29,19 @@ def _maindriver():
         resetRobot()
         print("Waiting for right button press...")
         button = waitForButtonPress()
-        print(button)
-
-        # display counter
-        # if the left button is pressed 
-        # counter=counter+1
-        # if right button is pressed
-        # run counter "continue counter from there"
 
         if button==Button.LEFT:
             counter=counter+1
             continue
 
-# the next few lines are the else loop here it is also the case if the right button is pressed.
+        # the next few lines are the else loop here it is also the case if the right button is pressed.
         if (counter != 1):
             arm_change_end_time = stopwatch.time()    
             print("Time for arm change time(ms): {}".format(str(arm_change_end_time- arm_change_start_time)))
 
         if counter == 1:
             # Sound Mixer
+            start_time = stopwatch.time()
             runWithTiming(run1,"run1")
             # runWithTiming(run2,"run2")
         if counter == 2:
@@ -69,12 +65,10 @@ def _maindriver():
         
         counter = counter + 1
         drive_base.straight(distance=0, then=Stop.BRAKE)
+    
+    end_time = stopwatch.time()
+    print("Total Time: " + str((end_time-start_time)/1000) + " seconds")
+
 
 initializeAndWaitForRobotReady()
-stopwatch = StopWatch()
-start_time = stopwatch.time()
 _maindriver()
-
-#run4()
-end_time = stopwatch.time()
-print("Total Time: " + str((end_time-start_time)/1000) + " seconds")
