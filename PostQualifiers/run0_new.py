da80a1b6eace9921d701ea7148668e278b589562
Nami <vr@example.com>
2024-01-05 20:36:25 -0800

    Renaming runs 0,1,2

---
+++ PostQualifiers/run0_new.py
@@ -0,0 +1,49 @@
+from Utilities import *
+
+TURN_SPEED = 300
+
+def _doSoundMixerWithComplicatedArm():
+    angle = 0
+    gyroStraightWithDriveWithAccurateDistance(distance = 35, speed = 800, targetAngle = angle, stop = Stop.COAST)
+    gyroStraightWithDriveWithAccurateDistance(distance = 20, speed = 400, targetAngle = angle)
+
+    # Turn the motor to remove the lock for the left sound mixer.
+    # Do this in parallel with the expert pick up.
+    left_med_motor.run_angle(speed=2000, rotation_angle=-800, wait=False)
+
+    # Turn the right motor to pick up the expert
+    right_med_motor.run_angle(speed=2000, rotation_angle=-800)
+
+    # Make sure that the stopper has been removed.
+    while left_med_motor.done() == False:
+        continue
+
+    # Now backoff.
+    gyroStraightWithDriveWithAccurateDistance(distance = 23, speed = 1000, targetAngle = angle, backward=True,
+                                              stop = Stop.COAST)
+
+    # Now drive back home.
+    # We start resetting the left motor when we curve home, because we want to be able to change the 
+    # attachment
+    _resetLeftMotor(wait = False)
+    drive_base.settings(500, 1000, 500, 1000)
+    drive_base.curve(radius = -500, angle = -40)
+
+def _resetBucket():
+    # Turn the right motor to pick up the expert
+    right_med_motor.run_angle(speed=2000, rotation_angle=800, wait = False)
+
+def _resetLeftMotor(wait):
+    left_med_motor.run_angle(speed=2000, rotation_angle=800, wait=wait)
+
+def waitForLeftMotor():
+    while left_med_motor.done() == False:
+        continue
+
+def run2():
+    resetRobot()
+    _doSoundMixerWithComplicatedArm()
+    _resetBucket()
+
+#runWithTiming(run2, "Sound Mixer")
+#_resetBucket()
---
+++ PostQualifiers/run1_new.py
@@ -0,0 +1,18 @@
+from Utilities import *
+
+def _3dCinemaAndTwoFacesDropoff():
+    # Go forward to push the static arm, which then aligns against the 3D Cinema and drops off one audience in the two faces.
+    #Alignment: To the left of the third black line from the wall. The attachment's yellow L-beam is pushed against the base arms yellow slope
+    angle = 0
+    gyroStraightWithDrive(distanceInCm = 25, speed = 300, targetAngle = angle) 
+    gyroStraightWithDrive(distanceInCm = 25, speed = 600, targetAngle = angle,backward=True) 
+
+def run0():
+    resetRobot()
+    _3dCinemaAndTwoFacesDropoff()
+
+#runWithTiming(run0,"3DCinema")
+
+#runWithTiming(_run0,"3DCinema")
+#runWithTiming(_run0point5,"Lever")
+#_run0point5()
