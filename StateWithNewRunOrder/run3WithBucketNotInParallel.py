f63206723fc84f70ecc8a92fa018dcaaf7a83157
Nami <vr@example.com>
2024-04-14 19:19:53 -0700

    slowed down run3 go home second curve

--- StateWithNewRunOrder/run3WithBucketNotInParallel.py
+++ StateWithNewRunOrder/run3WithBucketNotInParallel.py
@@ -43,11 +43,10 @@ def _doSceneChangeBackup():
     angle = 25
     # Backup after expert dropoff
     drive_base.straight(distance=-80)
-
     # Start bringing down the bucket when we move forward to push the scene.
     angle = -45
     turnToAngle(targetAngle=angle, speed=500)
-    gyroStraightWithDrive(distanceInCm=8, targetAngle=angle, speed=300)
+    gyroStraightWithDrive(distanceInCm=9, targetAngle=angle, speed=300) # recent change. was 8
     drive_base.straight(-20)
     right_med_motor.run_angle(speed=2000, rotation_angle = -800, wait = False)
     #waitForButtonPress()
@@ -62,9 +61,11 @@ def _doSceneChangeBackup():
 
 def goHome():
     # Go home with curve
-    drive_base.settings(1000, 1000, 1000, 1000)
+    drive_base.settings(300, 300, 300, 300)
     #Change 1/24/2024: radius change from -155
     drive_base.curve(radius = -50, angle = -45, then=Stop.COAST)
+
+    drive_base.settings(1000, 1000, 1000, 1000)
     #Radius was -650 and angle was -50
     drive_base.curve(radius = -720, angle = -55)
 
