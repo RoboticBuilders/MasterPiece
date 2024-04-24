2ffea03b6465924da9a958d5980bcbf67118a902
Anya <anya.rajyaguru@gmail.com>
2024-04-23 19:33:14 -0700

    commented code for run0 to use load when picking up Izzy

--- StateWithNewRunOrder/run0.py
+++ StateWithNewRunOrder/run0.py
@@ -16,10 +16,19 @@ def run0():
 
         # drive forward before picking up arm
         #gyroStraightWithDrive(distanceInCm = 22, speed = 500, targetAngle = angle)
-        gyroStraightWithDriveWithAccurateDistance(distance = 29, speed = 300, backward = False, targetAngle = 0)
+        gyroStraightWithDriveWithAccurateDistance(distance = 29, speed = 300, backward = False, targetAngle = angle)
 
         # Pickup the expert
-        run6PositionPickUpExpertAttachment(position=RUN6_PICKUP_EXPERT_ATTACHMENT_UP, speed = 150)
+        # run6PositionPickUpExpertAttachment(position=RUN6_PICKUP_EXPERT_ATTACHMENT_UP, speed = 150, wait = False)
+        # passed_load_threshold = False
+        # while right_med_motor.done() == False:
+        #     if right_med_motor.load() > 40:
+        #         hub.speaker.beep()
+        #         passed_load_threshold = True
+        # if passed_load_threshold == False:
+        #     print("Missed Izzy")
+        # else:
+        #     print("Picked up Izzy")
 
     def _doChickenAndCraftCretor():
         angle = 0
